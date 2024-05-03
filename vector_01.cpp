2fb290fd9cfc97a9478ecfaa950acc1ea0279e69
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-05-03 12:17:53 +0600

    Create vector_01.cpp

---
+++ vector_01.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>t;
+    t.push_back(2);
+    t.push_back(4);
+    t.push_back(6);
+    t.push_back(8);
+    t.push_back(10);
+    cout<<t[2]<<endl;
+    for(int i=0;i<5;i++){
+        cout<<t[i]<<endl;
+    }
+}
