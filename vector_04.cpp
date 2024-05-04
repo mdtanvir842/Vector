f5ed91d3018fe962966462f1b036d2c34cb0b4c2
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-05-04 15:23:22 +0600

    Add files via upload

---
+++ vector_04.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    /*vector<int>v={2,4,6,8,10};
+    cout<<v.size()<<endl;
+    v.resize(10);
+    v[8]=11;
+    cout<<v.size()<<endl;
+
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<endl;
+    }*/
+    //another
+    vector<int>v1;
+    v1.resize(10);
+    for(int i=0;i<v1.size();i++){
+        cin>>v1[i];
+    }
+    for(int i=0;i<v1.size();i++){
+            cout<<v1[i]<<endl;
+    }
+}
