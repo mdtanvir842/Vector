eb513171d76a4fc7d58a0e67bfb4ff812d29f90c
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-05-04 15:22:35 +0600

    Add files via upload

---
+++ vector_03.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>v={2,4,8,1,3};
+    v[0]=10;
+    cout<<v.size()<<endl;
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<endl;
+    }
+}
