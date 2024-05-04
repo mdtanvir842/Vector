0212fbfbed34cdf886e129a7fa1d3acadc7d88aa
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-05-04 15:24:40 +0600

    Add files via upload

---
+++ vector_06.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>v={2,4,6,8,10};
+    for(int u:v){
+        cout<<u<<endl;
+    }
+    vector<int>v1={1,2,3,4,5,6,7,8,9,10};
+    for(auto i:v1){
+        cout<<i<<endl;
+    }
+}
