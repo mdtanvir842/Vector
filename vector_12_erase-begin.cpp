aff24c58316c5c7013794317b27d6cb91a346cc3
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-05-11 16:53:34 +0600

    Create vector_12_erase-begin.cpp

---
+++ vector_12_erase-begin.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>v={52,4,89,4,4,2,6,8,10};
+    cout<<*(v.begin())<<endl;
+    v.erase(v.begin());
+    cout<<*(v.begin())<<endl;
+    //cout<<*(v.begin()+2)<<endl;
+    //v.erase(v.begin()+2);
+    //cout<<*(v.begin()+2)<<endl;
+    //cout<<v.front()<<endl;
+
+
+}
