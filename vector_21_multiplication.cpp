382d5599a760280da24adbba27b5ab3b2431edbb
Md Tanvir Alam <mdtanviralam842@gmail.com>
2025-03-09 11:51:42 +0600

    Create vector_21_multiplication.cpp

---
+++ vector_21_multiplication.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	vector<int>v={4,6,8,10,12,13};
+	int x=accumulate(v.begin(), v.end(), 1, std::multiplies<int>());
+	cout<<x<<endl;
+
+}
